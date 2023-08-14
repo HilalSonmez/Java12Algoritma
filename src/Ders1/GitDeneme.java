4c4301044bbba95b67ef2e46294a4cbbfe20b100
HilalSonmez <126201797+HilalSonmez@users.noreply.github.com>
2023-08-14 16:41:48 +0300

    hamfiyat hesap

--- src/Ders1/GitDeneme.java
+++ src/Ders1/GitDeneme.java
@@ -1,9 +1,19 @@
 package Ders1;
 
+import java.util.Scanner;
+
 public class GitDeneme {
 
 	public static void main(String[] args) {
-		System.out.println("Github Deneme");
+	int urunFiyat;
+	double hamFiyat;
+	
+	Scanner scanner=new Scanner(System.in);
+	System.out.println("Ürün fiyatı giriniz");
+	urunFiyat=scanner.nextInt();
+	
+	hamFiyat=urunFiyat/(1.18*1.15);
+	System.out.println("ham fiyat= " +hamFiyat);
 
 	}
 

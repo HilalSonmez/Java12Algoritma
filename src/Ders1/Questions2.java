17243d3e7eeb44edd00a96aa94d138a203501640
HilalSonmez <126201797+HilalSonmez@users.noreply.github.com>
2023-08-15 16:38:40 +0300

    Ders2

--- src/Ders1/Questions2.java
+++ src/Ders1/Questions2.java
@@ -1,10 +1,19 @@
 package Ders1;
 
+import java.util.Scanner;
+
 public class Questions2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner= new Scanner(System.in);
+		String isim,soyisim;
+		
+		System.out.println("İsim gir");	
+		isim=scanner.nextLine();
+		;System.out.println("Soyisim gir");
+		soyisim=scanner.nextLine();
+		System.out.println("hoşgeldiniz "+isim+" "+soyisim);
+		  
 
-	}
-
+}
 }
